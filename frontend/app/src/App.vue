e87c979e99238149a7b8a83d3ffcedb657c0caf1
Nyashadzashe Pongolani <78688@office.mans.org.pl>
2026-03-17 11:22:57 +0100

    Project 1: scaffold Laravel backend and Vue frontend

---
+++ frontend/app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Web systems design</h1>
+  <p>
+    Name: Nyashadzashe Pongolani<br>
+    ID: 78688<br>
+    DPH57
+  </p>
+</template>
+
+<style scoped></style>
